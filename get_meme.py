49a984942dcd2d4dce69adb4469f1e1bcf7e7074
8Dion8 <shvartserinfo@gmail.com>
2022-02-16 11:02:32 +0000

    basic auth

--- get_meme.py
+++ get_meme.py
@@ -1,5 +1,18 @@
 import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("id")
+client_secret = os.getenv("secret")
+password = os.getenv("accpass")
 
 reddit = praw.Reddit(
-    
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent="User-Agent: script:memes-grabber:v0.0.1 (by /u/GameOfShadows)",
+    username="GameOfShadows",
+    password=password
 )
+
